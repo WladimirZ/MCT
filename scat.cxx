02a3bb69ae2369ba0d3ade3e495a27906212cb46
Wladimir <wladimir@linux-qhme.site>
2016-03-01 13:00:49 +0100

    yo

---
+++ scat.cxx
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+void path
+
+int main() {
+  
+  const int N = 100000;
+  const double d = 1;
+  const int Gn = 100;
+  const double v0 = 0.01;
+  const double v1 = 1;
+  const double Ra = 0.01;
+  const double Rs = 0.3;
+  double lambda0 = v0 / (Ra + Rs);
+  double lambda1 = v1 / (Ra + Rs);
+  
+  return 0;
+}
